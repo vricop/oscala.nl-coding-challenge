cf3e87147092b19d0194ba99b48ffd5f31c07be3
Víctor Rico <vricop@gmail.com>
2020-10-27 21:38:17 +0100

    Create boilerplate project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-center uppercase tracking-wide | pt-8 m-0">Oscala.nl test...</h1>
   );
 }
-
-export default App;
