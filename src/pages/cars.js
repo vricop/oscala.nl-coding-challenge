2451807f89d57a118dcfba73db0763cc9590a0f4
Víctor Rico <vricop@gmail.com>
2020-10-30 17:36:29 +0100

    Remove comment

--- src/pages/cars.js
+++ src/pages/cars.js
@@ -31,31 +31,6 @@ const Cars = () => {
 
       { (layout == 'grid') && <CarCardContainer data={data} /> }
       { (layout == 'list') && <CarBlockContainer data={data} /> }
-      { 
-        // # GOAL
-        //
-        // Create a folder structure for car components
-        //
-        // ./components
-        //  +--/cars
-        //     +--car-card.js  <== Layout 1
-        //     +--car-block.js <== Layout 2
-        //     +--index.js <== 1. Import both layouts and wrapp'em with different
-        //                        containers (grid/list)
-        //                     2. Export <CarCardContainer /> and
-        //                        <CarBlockContainer /> components
-        //
-        // 1. In './pages/cars' import '../components/cars
-        // 2. Display <CarCardContainer /> or <CarBlockContainer /> based on a
-        //   $layout state (posible values are 'grid' & 'list').
-        // 3. Then based on $layout state show one component or the other
-        // 4. And pass JSON data via `data` prop. e.g
-        // 
-        // if $layout is 'grid' => <CarCardContainer data={JSONData} />
-        // otherwise => <CarBlockContainer data={JSONData} />
-        //
-        // **Note**: $layout state will be updated from two different buttons
-      }
     </section>
   )
 }
